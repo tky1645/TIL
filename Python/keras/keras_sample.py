025628720c4fc4e726ff21178e406cbb283810e7
chinchillaa <2gawasa10ru@gmail.com>
2024-01-21 15:18:25 +0900

    blacked keras_sample.py

--- Python/keras/keras_sample.py
+++ Python/keras/keras_sample.py
@@ -4,7 +4,6 @@ from tensorflow.keras.layers import Dense
 # Create a new sequential model
 model = Sequential()
 # Add and input and dense layer
-model.add(Dense(2, input_shape=(3,),
-                activation="relu"))
+model.add(Dense(2, input_shape=(3,), activation="relu"))
 # Add a final 1 neuron layer
 model.add(Dense(1))
