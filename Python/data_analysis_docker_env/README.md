1867a908436c16da7b8ccc7be353bacdf47c5dad
chinchillaa <2gawasa10ru@gmail.com>
2024-02-10 02:19:00 +0000

    Enhanced Docker env, added torchvision, and updated PyTorch notebook

---
+++ Python/data_analysis_docker_env/README.md
@@ -0,0 +1,4 @@
+# 構成
+
+`/home/chinchilla/Python/data_analysis_docker_env/.devcontainer`に主要なファイルを配置している。
+``
--- Python/keras/keras_sample.py
+++
@@ -1,9 +0,0 @@
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-
-# Create a new sequential model
-model = Sequential()
-# Add and input and dense layer
-model.add(Dense(2, input_shape=(3,), activation="relu"))
-# Add a final 1 neuron layer
-model.add(Dense(1))
